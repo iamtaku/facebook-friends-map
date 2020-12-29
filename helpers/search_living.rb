10a991e64d28256f0679d559f175b3f520691825
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-29 13:16:44 +0900

    began excaptions

--- helpers/search_living.rb
+++ helpers/search_living.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 
-file = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000090896803.html'
+# file = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000090896803.html'
+file = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000187815559.html'
 
 html_file = open(file).read
 html_doc = Nokogiri::HTML(html_file)
@@ -9,14 +10,19 @@ root = html_doc.search('.bi.bj')
 def search_living(root)
   # living = {}
   root_living = root.search('#living')
-  city = root_living.css('.ec a')
-
+  # puts root_living
+  city = root_living.css('table .ec')
+  puts city
+  # puts city[0].content
+  cities = city.map{ |c| c.content.nil? ? '' : c.content }
+  puts cities
   # homeTown = root_living.css('.')
-  living = {
-    currentCity: city[0].content,
-    homeTown: city[1].content,
-  }
-  puts living
+    living = {
+      currentCity: cities[0],
+      homeTown: cities[1],
+    }
+
+    puts living
 end
 
 search_living(root)
