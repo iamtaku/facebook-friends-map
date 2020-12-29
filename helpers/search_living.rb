251a4f3b5561c2ce1a54cb6d2e1481bccd9d5055
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-29 20:11:43 +0900

    updated db + progress on search_living helpers

--- helpers/search_living.rb
+++ helpers/search_living.rb
@@ -1,28 +1,34 @@
 require 'nokogiri'
 
-# file = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000090896803.html'
-file = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000187815559.html'
+file1 = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000090896803.html'
+file2 = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000187815559.html'
+file3 = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000126333972.html'
+files = [file1, file2, file3]
 
-html_file = open(file).read
-html_doc = Nokogiri::HTML(html_file)
-root = html_doc.search('.bi.bj')
+# html_file = open(file).read
+# html_doc = Nokogiri::HTML(html_file)
+# root = html_doc.search('.bi.bj')
 
-def search_living(root)
-  # living = {}
+def search_living(root_file)
+  living = {
+    currentCity: '',
+    homeTown: ''
+  }
+  html_file = open(root_file).read
+  html_doc = Nokogiri::HTML(html_file)
+  puts html_doc.search('title')
+  root = html_doc.search('.bi.bj')
   root_living = root.search('#living')
-  # puts root_living
-  city = root_living.css('table .ec')
-  puts city
-  # puts city[0].content
-  cities = city.map{ |c| c.content.nil? ? '' : c.content }
-  puts cities
-  # homeTown = root_living.css('.')
-    living = {
-      currentCity: cities[0],
-      homeTown: cities[1],
-    }
-
-    puts living
+  city_table = root_living.css('tr')
+  city_table.each { |item| puts item.nil? }
+  cities = city_table.map { |c| c.content.nil? ? '' : c.content }
+  # puts cities
+  living = {
+    currentCity: cities[0],
+    homeTown: cities[1],
+  }
 end
 
-search_living(root)
+
+files.each {|file| search_living(file)}
+# search_living(root)
