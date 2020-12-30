7226d9a6ed1d1b2cf9d8039eef31076b0cd14532
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-30 13:11:38 +0900

    updated get_lived_in function to scrape location and year

--- helpers/search_living.rb
+++ helpers/search_living.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'pry'
 
 file1 = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000090896803.html'
 file2 = '/Users/takuheimizutani/code/iamtaku/facebook-friends-map/db/profiles/100000187815559.html'
@@ -9,25 +10,51 @@ files = [file1, file2, file3]
 # html_doc = Nokogiri::HTML(html_file)
 # root = html_doc.search('.bi.bj')
 
-def search_living(root_file)
-  living = {
-    currentCity: '',
-    homeTown: ''
+def search_location(root_file)
+  location = {
+    lived_in: nil,
+    home: nil
   }
   html_file = open(root_file).read
   html_doc = Nokogiri::HTML(html_file)
   puts html_doc.search('title')
   root = html_doc.search('.bi.bj')
   root_living = root.search('#living')
-  city_table = root_living.css('tr')
-  city_table.each { |item| puts item.nil? }
-  cities = city_table.map { |c| c.content.nil? ? '' : c.content }
-  # puts cities
-  living = {
-    currentCity: cities[0],
-    homeTown: cities[1]
+  return location unless root_living.any?
+
+  # puts root_living
+  lived_in = get_lived_in(root_living)
+  puts lived_in
+  # 'this was returned'
+end
+
+def get_lived_in(root)
+  lived_in = []
+  home = {}
+  city_table = root.search('td')
+  city_table[3..-1].each_with_index do |item, index|
+    place = {
+      year: 'current'
+    }
+    # item.text = item.text
+    puts "TD: #{item.text}"
+
+    break if item.text == 'Hometown'
+
+    if city_table[3..-1][index - 1].text.chars.first(8).join == 'Moved in' && index.positive?
+      place[:year] = city_table[3..-1][index - 1].text.chars.slice(9..item.text.length).join.to_i
+    end
+
+    next if item.text.chars.first(8).join == 'Moved in'
+
+    place[:name] = item.text
+    lived_in << place
+  end
+  home[:name] = city_table.last.text
+  {
+    lived_in: lived_in,
+    home: home
   }
 end
 
-files.each { |file| search_living(file) }
-# search_living(root)
+files.each { |file| search_location(file) }
