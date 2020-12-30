ce4ae8dc57e1e61d6925a44d917f867a31ea15dd
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-30 13:56:46 +0900

    get_location working pre-refactoring

--- helpers/search_living.rb
+++ helpers/search_living.rb
@@ -26,10 +26,35 @@ def search_location(root_file)
 end
 
 def get_location(root)
-  lived_in = []
+  # lived_in = []
   home = {}
   city_table = root.search('td')
-  city_table[3..].each_with_index do |item, index|
+  # city_table[3..].each_with_index do |item, index|
+  #   break if item.text == 'Hometown'
+  #   # check if each <td> has a year attached and attach to place object if attached
+
+  #   next if item.text.chars.first(8).join == 'Moved in'
+
+  #   year = get_year(city_table, index)
+  #   # place[:name] = item.text
+  #   lived_in << {
+  #     name: item.text,
+  #     year: year
+  #   }
+  # end
+  lived_in = create_lived_in(city_table)
+  home[:name] = city_table.last.text
+  {
+    lived_in: lived_in,
+    home: home
+  }
+  # return location hash
+  # need lived_in
+end
+
+def create_lived_in(city_table)
+  lived_in = []
+   city_table[3..].each_with_index do |item, index|
     break if item.text == 'Hometown'
     # check if each <td> has a year attached and attach to place object if attached
 
@@ -42,32 +67,8 @@ def get_location(root)
       year: year
     }
   end
-  home[:name] = city_table.last.text
-  {
-    lived_in: lived_in,
-    home: home
-  }
-  # return location hash
-  # need lived_in
+  lived_in
 end
-
-# def create_lived_in(city_table)
-#   lived_in = []
-#   city_table[3..].each_with_index do |item, index|
-#     break if item.text == 'Hometown'
-#     # check if each <td> has a year attached and attach to place object if attached
-
-#     next if item.text.chars.first(8).join == 'Moved in'
-
-#     year = get_year(city_table, index)
-#     # place[:name] = item.text
-#     {
-#       name: item.text,
-#       year: year
-#     }
-#   end
-#   lived_in
-# end
 
 def get_year(city_table, index)
   year = nil
