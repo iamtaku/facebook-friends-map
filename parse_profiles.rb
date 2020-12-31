0475b1589e08132a7ca85e4cefc2ef3529f8c5a0
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-31 22:10:17 +0900

    json is now working!

--- parse_profiles.rb
+++ parse_profiles.rb
@@ -1,15 +1,33 @@
 require 'nokogiri'
+require 'json'
 require_relative 'helpers/search_living'
 require_relative 'helpers/make_profile'
 
 puts "Scanning #{Dir['db/profiles/*'].length} files \n ... \n"
 sleep 2
 
-Dir.glob('db/profiles/*.html') do |file|
+# File.open("log.txt", "w") do |f|
+  # f.write "#{Time.now} - User logged in\n" end
+# open a file
+# append contents
+
+
+
+json = Dir.glob('db/profiles/*.html').map do |file|
   html_file = open(file).read
   id = file.match(/(\d+)/)[0].to_i
   html_doc = Nokogiri::HTML(html_file)
   profile = make_profile(id, html_doc)
-  puts profile
+  # puts "parsing #{profile[:name]}..."
+  JSON.generate(profile)
 end
+json_profiles = JSON.generate({key: json})
+
+# p json_profiles
+File.write("test.json", json_profiles, mode: "w")
+
+# File.open('test.json','w') do|f|
+#   my_hash = JSON.parse(f)
+#   puts my_hash
+# end
 
