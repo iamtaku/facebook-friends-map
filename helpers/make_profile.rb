efab8a389b45c171313a020c8f4b6808543e5fe9
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-31 21:25:22 +0900

    modified helper/make_profile attributes

--- helpers/make_profile.rb
+++ helpers/make_profile.rb
@@ -1,7 +1,10 @@
 require_relative 'search_living'
 
-def make_profile(root)
+def make_profile(root_file)
+  root = root_file.search('.bi.bj')
+  name = root_file.search('title').text
   profile = {
+    name: name,
     living: search_location(root)
   }
 end
